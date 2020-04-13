850bdfb296e14db54b27d8a92490b5d4e07aa323
Dhanesh U <dhanesh.u@sainsburys.co.uk>
2020-04-13 12:18:46 +0100

    First commit

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,24 +1,20 @@
+
+
 provider "azurerm" {
-  subscription_id = "09063c76-8a3d-40ed-a277-f64e1cc38d84"
-  tenant_id       = "e11fd634-26b5-47f4-8b8c-908e466e9bdf"
-  features {}
+  subscription_id = "<SubscriptionID_where you want to build>"
+  tenant_id       = "<Tenant ID>"
 }
 
-# provider "azurerm" {
-#   subscription_id = "<SubscriptionID_where you want to build>"
-#   tenant_id       = "<Tenant ID>"
-# }
-
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "<statefile_rg_name>"
-#     storage_account_name = "<statefile_storage_account_name>"
-#     container_name       = "<statefile_container_name>"
-#     key                  = "statefilename.tfstate"
-#     subscription_id      = "<statefile_subscription_ID>"
-#     tenant_id            = "<Tenant ID>"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "<statefile_rg_name>"
+    storage_account_name = "<statefile_storage_account_name>"
+    container_name       = "<statefile_container_name>"
+    key                  = "statefilename.tfstate"
+    subscription_id      = "<statefile_subscription_ID>"
+    tenant_id            = "<Tenant ID>"
+  }
+}
 
 terraform {
   required_version = ">= 0.12.3"
